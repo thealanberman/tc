bbaf2a628baefd58bb5aa2b938152cf8c9dda460
Andrea Stella <adr.stella@gmail.com>
2015-05-24 18:53:59 -0600

    make download buttons work

---
+++ js/download-buttons.js
@@ -0,0 +1,12 @@
+$.getJSON('https://api.github.com/repos/mccxiv/tc/releases?callback=?', function(response) {
+	if (response) {
+		var releases = ['tc-windows', 'tc-setup', 'tc-osx', 'tc-linux'];
+		response.data[0].assets.forEach(function(asset) {
+			releases.forEach(function(release) {
+				if (asset.name.indexOf(release) === 0) {
+					$('.'+release).attr('href', asset.browser_download_url);
+				}
+			})
+		});
+	}
+});
