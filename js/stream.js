2722320f74f567ac45b1aab60f66354011e6fd69
Andrea Stella <adr.stella@gmail.com>
2015-08-28 08:25:20 -0600

    Replace video with static image, remove beta warning

--- js/stream.js
+++ js/stream.js
@@ -1,15 +1,9 @@
 $.getJSON('https://api.twitch.tv/kraken/streams?limit=1&callback=?', function(data) {
 	if (data) {
-		var streamer = data.streams[0].channel.name;
-		var obj = '<object class="video-object" type="application/x-shockwave-flash" height="255" width="400" bgcolor="#000000">'+
-			'<param name="allowFullScreen" value="true">'+
-			'<param name="allowScriptAccess" value="always">'+
-			'<param name="allowNetworking" value="all">'+
-			'<param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf">'+
-			'<param name="flashvars" value="hostname=www.twitch.tv&amp;channel='+streamer+'&amp;auto_play=true&amp;start_volume=0">'+
-			'<param name="wmode" value="opaque">'+
-			'</object>';
+		var preview = data.streams[0].preview.template;
+		preview = preview.replace('{width}', '1280');
+		preview = preview.replace('{height}', '720');
 
-		$('.video-section').append(obj);
+		$('.video-section').css('background-image', 'url('+preview+')');
 	}
 });
